3efbbeb020755291a5ce016c9be099b2f942472a
chananiiii <pchanwan@naver.com>
2021-02-26 18:27:24 +0900

    주석 추가

---
+++ algo/src/BFS/baekjoon_9205.java
@@ -0,0 +1,19 @@
+package BFS;
+import java.util.*;
+import java.io.*;
+public class baekjoon_9205 {
+	public static Deque <> deque = 0;
+	public static ArrayList <ArrayList<Integer>> graph = null;
+	public static void main(String [] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int testcase = Integer.parseInt(br.readLine());
+		for(int i = 0; i < testcase; i ++) {
+			int storeNum = Integer.parseInt(br.readLine());
+			graph.add(new ArrayList<>());
+			for(int j = 0; j < storeNum; j ++) {
+				
+			}
+		}
+	}
+}
---
+++ algo/src/JAVAContents/contents.java
@@ -0,0 +1,148 @@
+package JAVAContents;
+/*
+■ 클래스, 생성자
+class Pair() {
+	int sero;
+	int garo;
+	// 생성자
+	public Pair(int sero, int garo) {
+		this.sero = sero;
+		this.garo = garo;
+	}
+	public int getSero() {
+		return this.sero;
+	}
+	public int getGaro() {
+		return this.garo;
+	} 
+}
+
+------------------------------------------------------------------------------------------
+
+■ 입출력
+BufferedReader br = new BufferedReader(InputStreamReader(System.in));
+BufferedWriter bw = new BufferedReader(OutputStreamWriter(System.out));
+String a = br.readLine();
+String temp = bf.readLine();
+//1,2,3,4 4개로 쪼개고 싶으면 new StringTokenizer(temp, ","); 로하면 자를 준비 완료.
+StringTokenizer st = new StringTokenizer(temp); // new StringTokenizer(temp, " ") 와 같다
+for(st.hasMoreTokens) {
+	int a = Integer.parseInt(st.nextToken());
+	int b = Integer.parseInt(st.nextToken());
+}
+bw.write(a + "\n");
+bw.flush();
+bw.close();
+
+------------------------------------------------------------------------------------------
+
+■ ArrayList
+★ ArrayList 에서 contains사용가능.
+import java.util.*;
+ArrayList <String> arrayList = new ArrayList();
+arrayList.add("test");
+arrayList.contains("test"); // true : 1, false : 2
+arrayList.remove(0); // 0번째 index 삭제
+
+★ 2차원 ArrayList 선언 및 사용(인접 그래프에서 많이 사용한다. Memory 절약)
+ArrayList <Integer> arrayList = new ArrayList[5];
+for(int i = 0; i < arrayList.size(); i ++) {
+	arrayList[i] = new ArrayList<Integer>();
+}
+
+------------------------------------------------------------------------------------------
+
+■ Deque(Queue, Stack)
+Deque <Integer> deque = new LinkedList();
+
+//queue 사용방법 0 1 2 FIFO first in first out
+deque.add(0);
+deque.add(1);
+deque.add(2);
+bw.write(deque.pollFirst() + "\n");
+bw.write(deque.pollFirst() + "\n");
+bw.write(deque.pollFirst() + "\n");
+	
+// stack 사용 방법 2 1 0 LIFO last in first out
+deque.add(0);
+deque.add(1);
+deque.add(2);
+bw.write(deque.pollLast() + "\n");
+bw.write(deque.pollLast() + "\n");
+bw.write(deque.pollLast() + "\n");
+
+------------------------------------------------------------------------------------------
+
+■ Set
+[중복을 허용하지 않는다는 점이 있다.]
+Set<String> set = new HashSet<String>();
+set.add("A");
+set.remove("A");
+
+
+출력의 2가지 방법(for, 단축for, iterator : iterator가 젤느리다 객체 생성하고 해야해서)
+Set<String> alpha = new HashSet<>();
+for(String item: str){
+        	alpha.add(item);
+}
+
+Iterator<String> iter = alpha.iterator();
+while(iter.hasNext()){
+	System.out.print(iter.next() + " ");
+}
+
+------------------------------------------------------------------------------------------
+
+■ HashMap
+★ 생성 및 사용
+HashMap <String, Integer> a = new HashSet<Long, Integer>();
+a.put("2", 3);
+a.get("2"); // Output : 3
+
+if(a.containsKey("2")); // TRUE
+if(a.containsValue("3")); // TRUE
+
+// key 값만 받아올때
+for(String a : a.keySet())
+// value값만 받아올때
+for(String a : a.values()) 
+
+------------------------------------------------------------------------------------------
+
+■ 문자열
+★char to int
+String temp = "123456"
+int sum = 0;
+for(int i = 0; i < temp.length; i ++) {
+	// This is Char to Int
+	sum += temp.charAt(i) - '0';
+}
+
+★int to String
+int a = 1;
+String b = Integer.toString(a);
+
+★String to int
+String temp = "123456"
+int a = Integer.parseInt(temp);
+
+★String 각 자리 나누기
+String temp1 = "test"
+// 방법1
+String [] temp2 = temp.split("");
+// 방법2
+for(int i = 0; i < temp.length; i ++) {
+	String a = temp.substring(i, i + 1);
+}
+
+★Sort함수(length를 기준으로 정렬하고, compareTo를 통해서 문자열 순으로 정렬한다.)
+Collections.sort(array, new Comparator<String>() {
+	@Override
+            public int compare(String a, String b) {
+		if(a.length() == b.length()) {
+			return a.compareTo(b);
+               	}
+		return a.length() - b.length();
+	}
+});
+*/
